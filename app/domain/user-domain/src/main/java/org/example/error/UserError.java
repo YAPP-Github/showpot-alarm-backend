7a56c4e23214bf67f0d4238278419549ed221feb
GaBaljaintheroom <junsu1222@naver.com>
2024-08-08 16:48:37 +0900

    feat : showPot Alarm 서버 CI 추가

--- app/domain/user-domain/src/main/java/org/example/error/UserError.java
+++ app/domain/user-domain/src/main/java/org/example/error/UserError.java
@@ -25,28 +25,5 @@ public enum UserError implements BusinessError {
         public String getLogMessage() {
             return "조회 대상 유저가 존재하지 않습니다.";
         }
-    },
-
-    // 1** 로그인
-    WITHDREW_USER_LOGIN {
-        @Override
-        public int getHttpStatus() {
-            return 400;
-        }
-
-        @Override
-        public String getErrorCode() {
-            return "USR-100";
-        }
-
-        @Override
-        public String getClientMessage() {
-            return "탈퇴한 유저는 로그인할 수 없습니다.";
-        }
-
-        @Override
-        public String getLogMessage() {
-            return "탈퇴한 유저는 로그인할 수 없습니다.";
-        }
     }
 }
