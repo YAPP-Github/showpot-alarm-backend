7614d48f7f7b308e41f5d730b5a2c550c493d841
devmizz <garamminchan@gmail.com>
2024-08-25 01:11:53 +0900

    feat: Message template

--- app/infrastructure/fcm/src/test/java/org/showpot/client/FCMClientTest.java
+++ app/infrastructure/fcm/src/test/java/org/showpot/client/FCMClientTest.java
@@ -1,7 +1,12 @@
 package org.showpot.client;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.example.message.MessageParam;
+import org.example.message.PushMessageTemplate;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.showpot.dto.param.FCMMessageParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -19,19 +24,27 @@ public class FCMClientTest {
     @Autowired
     private FCMClient fcmClient;
 
-    @Test
-    public void send() throws FirebaseMessagingException {
-        FCMMessageParam messageParam = FCMMessageParam.builder()
-            .title("title")
-            .body("body")
-            .build();
+    @ParameterizedTest
+    @MethodSource("getMessage")
+    public void send(MessageParam message) throws FirebaseMessagingException {
+        FCMMessageParam fcmMessage = FCMMessageParam.from(message);
 
         fcmClient.sendNotification(
             "cLGIPRQUS8WYOtGNiN0_jz:"
                 + "APA91bGBmeu5-"
                 + "A2aXvbrg8Kssnsh9KgmY0Z6miJB2Fa3E-"
                 + "g2JWGcbUNYaeJTSTgFoid9f9wYx3zZ40VMuQoYTPdroj3gtzb7cmALuDCOROakttyIr7wCVTbFEFJlTDPVNOF1uPD7Te1U",
-            messageParam.toNotification()
+            fcmMessage.toNotification()
+        );
+    }
+
+    private static Stream<Arguments> getMessage() {
+        return Stream.of(
+            Arguments.of(PushMessageTemplate.getTicketingAlertMessageBefore24Hours()),
+            Arguments.of(PushMessageTemplate.getTicketingAlertMessageBefore6Hours()),
+            Arguments.of(PushMessageTemplate.getTicketingAlertMessageBefore1Hours()),
+            Arguments.of(PushMessageTemplate.getSubscribedArtistVisitKoreaAlertMessage()),
+            Arguments.of(PushMessageTemplate.getSubscribedGenreVisitKoreaAlertMessage())
         );
     }
 }
--- common/src/main/java/org/example/message/PushMessageTemplate.java
+++ common/src/main/java/org/example/message/PushMessageTemplate.java
@@ -2,10 +2,41 @@ package org.example.message;
 
 public class PushMessageTemplate {
 
-    public static MessageParam getTestMessageTemplate() {
+    public static MessageParam getTicketingAlertMessageBefore1Hours() {
         return MessageParam.builder()
-            .title("메세지 모시깽이 타이틀")
-            .body("메세지 모시깽이 내용")
+            .title("0000 티켓팅이 1시간 남았어요!")
+            .body("1시간 후, 0000 예매가 시작됩니다!\n"
+                + "티켓팅을 잊지 말고 준비하세요\uD83C\uDF9F")
+            .build();
+    }
+
+    public static MessageParam getTicketingAlertMessageBefore6Hours() {
+        return MessageParam.builder()
+            .title("0000 티켓팅이 6시간 남았어요!")
+            .body("6시간 후, 0000 예매가 오픈됩니다!\n"
+                + "성공적인 티켓팅을 쇼팟이 응원해요\uD83E\uDD70")
+            .build();
+    }
+
+    public static MessageParam getTicketingAlertMessageBefore24Hours() {
+        return MessageParam.builder()
+            .title("0000 티켓팅이 24시간 남았어요!")
+            .body("24시간 후, 0000 예매가 오픈됩니다.\n"
+                + "놓치지 말고 티켓팅을 준비하세요\uD83D\uDE00")
+            .build();
+    }
+
+    public static MessageParam getSubscribedArtistVisitKoreaAlertMessage() {
+        return MessageParam.builder()
+            .title("속보! 0000의 내한 소식이 발표되었어요\uD83C\uDF8A")
+            .body("쇼팟에서 상세한 내한 정보를 확인해보세요!")
+            .build();
+    }
+
+    public static MessageParam getSubscribedGenreVisitKoreaAlertMessage() {
+        return MessageParam.builder()
+            .title("구독하신 00 장르의 공연 소식이 발표되었어요\uD83C\uDF88")
+            .body("쇼팟에서 상세한 공연 정보를 확인해보세요!")
             .build();
     }
 }
